ed0557f957be014f8ccd2f9cb0e313cf68e233c7
itc-s26006 <s26006@std.it-college.ac.jp>
2026-07-09 02:16:52 +0000

    3.6 whileを使ったプログラミング   ([問題1](chap03/q3_6_1.py)、[問題2](chap03/q3_6_2.py))~

---
+++ chap03/q3_6_1.py
@@ -0,0 +1,13 @@
+import random
+numberlist=['1','2','3','4','5','6','7','8','9']
+
+sample4=random.sample(numberlist,k=4)
+num4=''.join(sample4)
+while True:
+    val =input()
+    if val == num4:
+        print('OK')
+        break
+    else:
+        print(val,':NG')
+
