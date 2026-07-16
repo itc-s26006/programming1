a1d52677d341f4c8a3c0173b4f6e28e689b722c0
itc-s26006 <s26006@std.it-college.ac.jp>
2026-07-16 03:56:12 +0000

    4.4 関数とスコープ([問題1](chap04/q4_4_1.py)、[問題2](chap04/q4_4_2.py))

---
+++ chap04/q4_4_2.py
@@ -0,0 +1,11 @@
+vote_num= 0
+
+def vote(vote_n):
+    print("投票します")
+    vote_n =0
+    return vote_n
+def check_box(vote_n):
+    print("表の数は{}".format(vote_n))
+    return
+
+vote_num=vote(vote_num)
