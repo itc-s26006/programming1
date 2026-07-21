ab948e5c3ba33412c7890831ce172390d4db612f
itc-s26006 <s26006@std.it-college.ac.jp>
2026-07-21 03:37:00 +0000

    q4_6_1.py修正

--- chap04/q4_6_1.py
+++ chap04/q4_6_1.py
@@ -1,4 +1,6 @@
 str_num_List = map(lambda x:"{:04}".format(x),range(1,8))
-str_num_list
+str_num_List
 
-print(list(str_num_list))
+print(list(str_num_List))
+
+
--- chap04/q4_6_2.py
+++ chap04/q4_6_2.py
@@ -1,5 +1,6 @@
 import random
-def generate_student_date(num_students=10):
+
+def generate_students_data(num_students=10):
     '''生徒名,身長,体重からなるデータをランダムに生成する
 
     引数:num_students:生徒の人数を取る
@@ -7,21 +8,30 @@ def generate_student_date(num_students=10):
 
     データの内容
         生徒名 'nXx,XX は10から50の番号
-        身長150-190cm　からランダムに選んだ値
-        体重　50-80kg　からランダムに選んだ値
+        身長150-190cm からランダムに選んだ値
+        体重 50-80kg からランダムに選んだ値
     '''
-    students_data=[]
+    students_data = []
     for i in range(num_students):
-        name='n'+str(random.radint(10,50))
-        height = random.randint(150,190)
-        weight = random.randint(50,80)
-        students_data.append((name,height,weight))
-        if i == 0 : print('i,name,height,weight')
-        if i<2 or i ==num_students -1:
-            print(i,name,height,weight)
+        name = 'n' + str(random.randint(10, 50))  # radint -> randint
+        height = random.randint(150, 190)
+        weight = random.randint(50, 80)
+        students_data.append((name, height, weight))
+        
+        if i == 0:
+            print('i,name,height,weight')
+        if i < 2 or i == num_students - 1:
+            print(i, name, height, weight)
         elif i == 2:
             print('...')
-        return students_data
+            
+    return students_data  # forループの外に出す
 
-students_data = generate_students_data(10)
+students_data = generate_students_data(10)  # 関数名を統一
+print('\nsort by height')
+for student in students_by_height:
+    print(student)
 
+print('\nsort by weight')
+for student in student_by_weight:
+    print(student)
