f2050fc19a59b513e188b28668857341236440c8
itc-s26006 <s26006@std.it-college.ac.jp>
2026-07-21 03:05:00 +0000

    5.1 リストのメソッド([問題1](chap05/q5_1_2.py)、[問題2](chap05/q5_1_3.py))

---
+++ chap05/q5_1_3.py
@@ -0,0 +1,7 @@
+queue=[]
+queue.append(1)
+print('queue:',queue)
+queue.append(2)
+print('pop from queue 1st value:',queue.pop(0))
+print('pop from queue 2nd value:',queue.pop(0))
+print('queue:',queue)
