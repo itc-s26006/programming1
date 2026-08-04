9acaadef78da104f0429251b3888b71a27092d9a
itc-s26006 <s26006@std.it-college.ac.jp>
2026-08-04 01:16:44 +0000

    修正

---
+++ chap06/q6_1.1.py
@@ -0,0 +1,5 @@
+class cat:
+    cry="ニャー"
+    legs=4
+    is_animal=True
+    tama=
---
+++ q5_10_1.py
@@ -0,0 +1,7 @@
+x=30
+
+def is_number_of_positions_of_10(x):
+    return 10=<x<20
+
+(is_number_of_position_of_10(12)
+
