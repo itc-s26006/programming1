2435849d78e2bf7dcdb17725f5a976f844ee331f
itc-s26006 <s26006@std.it-college.ac.jp>
2026-08-19 01:17:26 +0000

    q6_1_1と2修正

--- chap06/q6_1.1.py
+++ chap06/q6_1.1.py
@@ -2,6 +2,7 @@ class cat:
     cry="ニャー"
     legs=4
     is_animal=True
-    tama=cat=()
-    "鳴き声:{},足の数,{},動物:{}".format(tama.cry,tama.cry.tama legs,tama.is_animal
+    
+tama=cat()
+print("鳴き声:{},足の数,{},動物:{}".format(tama.cry,tama.legs,tama.is_animal))
 
--- chap06/q6_1_2.py
+++ chap06/q6_1_2.py
@@ -1,14 +1,13 @@
 class Person:
     def __init__(self,
-                 name='',
-                 nationality='',
-                 birth='',
-                 address = '')
-
-    self.name=name
-    self.nationality =nationality
-    self.birth=birth
-    self.address=address
+                 name = '',
+                 nationality = '',
+                 birth = '',
+                 address = ''):
+        self.name = name
+        self.nationality = nationality
+        self.birth = birth
+        self.address = address
 
     def show_attributes(self):
         print("名前",self.name)
